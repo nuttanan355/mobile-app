da5e47a62925f800a2680e1c8c1152d1bca4b4f8
Nuttanan Jantaramanee <nuttanan355@gmail.com>
2021-08-05 17:21:27 +0700

    (JJ) Update

---
+++ dart/week2.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  // Scanning number
+  // print("Enter Year : = ");
+  // int? year = int.parse(stdin.readLineSync()!);
+  // print(callGen(year));
+
+  //var x = callColor("white");
+  //print(x);
+
+  callWeb("google", protocal: "https", port: 443);
+}
+
+//เรียก function
+// String callGen(int year) => (year >= 1996) ? "Gen Z" : "Gen Y";
+
+//<for in(foreach)>
+int callColor([String newColor = "pink"]) {
+  List<dynamic> color = ["red", "blue", "green"];
+
+  for (var item in color) {
+    print(item);
+  }
+  return color.length; //return จำนวนข้อมูลกลับ
+}
+
+//arrow function เป็นแบบ optinal ทั้ง position และ name
+void callWeb(String web, {int port = 80, String protocal = "http"}) =>
+    print("$protocal $web : $port");
