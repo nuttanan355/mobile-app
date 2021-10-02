3cb6681b0816bc97ab1ec17efcf6cd4aa61d13e7
Nuttanan Jantaramanee <nuttanan355@gmail.com>
2021-10-02 23:21:23 +0700

    (JJ) Register

--- lib/confin/constant.dart
+++ lib/confin/constant.dart
@@ -4,3 +4,5 @@ const pColor = Color(0xFF22577A);
 const sColor = Color(0xFF38A3A5);
 const tColor = Color(0xFF57CC99);
 const uColor = Color(0xFF80ED99);
+
+const boxSize = 20.0;
--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,22 +1,154 @@
+import 'package:app/confin/constant.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
-  const Register({ Key? key }) : super(key: key);
+  const Register({Key? key}) : super(key: key);
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
+  var name, surname, email, password;
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
-        title: Text("Register"),
+        title: Text('Register'),
       ),
-      body: Text("Hello"),
+      body: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                txtName(),
+                txtSurname(),
+                txtEmail(),
+                txtPassword(),
+                btnSubmit(),
+              ],
+            ),
+          )),
+    ));
+  }
 
-      
+  Widget txtName() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Name:',
+          icon: Icon(Icons.account_circle),
+          hintText: 'Input your name',
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'กรุณาใส่ข้อมูลด้วย';
+          } else if (value.length < 2) {
+            return 'กรุณาใส่ข้อมูลมากกว่า 2 ตัวอักษร';
+          }
+        },
+        onSaved: (value) {
+          name = value;
+        },
+      ),
     );
   }
+
+  Widget txtSurname() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Surname:',
+          icon: Icon(Icons.add_reaction_sharp),
+          hintText: 'Input your surname',
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'กรุณากรอกข้อมูล';
+          }
+        },
+        onSaved: (value) {
+          surname = value!.trim();
+        },
+      ),
+    );
+  }
+
+  Widget txtEmail() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Email:',
+          icon: Icon(Icons.email),
+          hintText: 'Input your email',
+        ),
+        validator: (value) {
+          if (!(value!.contains('@'))) {
+            return 'กรุณากรอกข้อมูลตามรูปแบบอีเมลด้วย';
+          } else if (!(value.contains('.'))) {
+            return 'กรุณากรอกข้อมูลตามรูปแบบอีเมลด้วย';
+          }
+        },
+        onSaved: (value) {
+          email = value;
+        },
+      ),
+    );
+  }
+
+  Widget txtPassword() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 20, 15, 20),
+      child: TextFormField(
+        obscureText: true,
+        style: TextStyle(
+          fontSize: 24,
+          color: pColor,
+        ),
+        decoration: InputDecoration(
+          labelText: 'Password:',
+          icon: Icon(Icons.lock),
+          hintText: 'Input your password',
+        ),
+        onSaved: (value) {
+          password = value;
+        },
+      ),
+    );
+  }
+
+  Widget btnSubmit() => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: pColor,
+        ),
+        onPressed: () {
+          print("Hello");
+          if (formKey.currentState!.validate()) {
+            formKey.currentState!.save();
+            print(
+                "Name : $name Surname : $surname Emai : $email Password : $password ");
+            formKey.currentState!.reset();
+          }
+        },
+        child: Text('Submit'),
+      );
 }
